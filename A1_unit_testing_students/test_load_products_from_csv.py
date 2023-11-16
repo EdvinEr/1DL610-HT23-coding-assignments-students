b31537419db9983c82ab304912e7af401a9a3d0c
EdvinEr <edvin.eriksson.6094@student.uu.se>
2023-11-16 22:32:15 +0100

    modified the csv file and tried testing if invalid inputs

--- A1_unit_testing_students/test_load_products_from_csv.py
+++ A1_unit_testing_students/test_load_products_from_csv.py
@@ -10,8 +10,17 @@ import pytest
 def copy_csv_file():
     shutil.copy('products.csv', 'copy_products.csv')
     products = load_products_from_csv('copy_products.csv')
+    #print("---------------------setup----------------")
     yield products
     os.remove('copy_products.csv')
+    #print("-teardown--")
+
+
+@pytest.fixture(scope='module')
+def modify_csv_file():
+    shutil.copy('products.csv', 'modify_products.csv')
+    yield 'modify_products.csv'
+    os.remove('modify_products.csv')
 
 
 def test_first_load_products_from_csv(copy_csv_file):
@@ -26,3 +35,19 @@ def test_sec_load_products_from_csv(copy_csv_file):
     assert products[1].name == 'Banana'
     assert products[1].price == 1.0
     assert products[1].units == 15
+
+
+def test_modify_load_products_from_csv(modify_csv_file):
+    with open(modify_csv_file, mode='w', newline='') as csvfile:
+        fields = ['Product', 'Price', 'Units']
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        writer.writeheader()
+        writer.writerow({'Product': 'Bread', 'Price': '-3.0', 'Units': '20'})
+        writer.writerow({'Product': 'Cheese', 'Price': '3.0', 'Units': '0'})
+        writer.writerow({'Product': 'Ham', 'Price': '3.0', 'Units': '-10'})
+
+    modified_products = load_products_from_csv(modify_csv_file)
+
+    assert modified_products[0].price == -3.0
+    assert modified_products[1].units == 0
+    assert modified_products[2].units == -10
