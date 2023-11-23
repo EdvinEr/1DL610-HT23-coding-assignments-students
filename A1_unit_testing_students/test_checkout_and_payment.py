99428e50eabf119df4941ebf00ff58ed56260592
smosebach <samo4544@student.uu.se>
2023-11-23 09:04:18 +0100

    9 tests

--- A1_unit_testing_students/test_checkout_and_payment.py
+++ A1_unit_testing_students/test_checkout_and_payment.py
@@ -1,4 +1,4 @@
-from checkout_and_payment import checkoutAndPayment
+from checkout_and_payment import checkoutAndPayment, ShoppingCart, Product
 from logout import logout
 import pytest
 import shutil
@@ -24,10 +24,6 @@ def check_cart_stub2(mocker):
 def logout_stub1(mocker):
     return mocker.patch('logout.logout', return_value=True)
 
-@pytest.fixture
-def logout_stub2(mocker):
-    return mocker.patch('logout.logout', return_value=False)
-
 def mimic_input(input_lst):
     i = 0
 
@@ -38,17 +34,60 @@ def mimic_input(input_lst):
         return mimicked_input
     return _mimic_input
 
-
-
-def test_choice_add_item(logout_stub1, capsys, monkeypatch): #Not done
+def test_printout_logout_confirmed(logout_stub1, capsys, monkeypatch):
     login_info = {"username": "Ramanathan", "wallet": 100}
-    monkeypatch.setattr("builtins.input", mimic_input(["28", "l"]))
+    monkeypatch.setattr("checkout_and_payment.products", [])
+    monkeypatch.setattr("builtins.input", mimic_input(["l"]))
     checkoutAndPayment(login_info)
     out, err = capsys.readouterr()
-    expected_output = "Ice cream added to your cart"
-    assert expected_output in out[2217:2249]
+    expected_output = "You have been logged out"
+    assert expected_output in out[:28]
 
-def test_choice_other_letter(logout_stub1, capsys, monkeypatch):
+def test_printout_one_product(logout_stub1, capsys, monkeypatch):
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    products = [Product("Ice cream", 10, 2)]
+    monkeypatch.setattr("checkout_and_payment.products", products)
+    monkeypatch.setattr("builtins.input", mimic_input(["l"]))
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()
+    expected_o = "1. Ice cream - $10.0 - Units: 2"
+    assert expected_o in out[:31]
+
+def test_printout_multiple_products(logout_stub1, capsys, monkeypatch):
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    products = [Product("Ice cream", 10, 2), Product("Chocolate", 15, 5), Product("Popcorns", 8, 3)]
+    monkeypatch.setattr("checkout_and_payment.products", products)
+    monkeypatch.setattr("builtins.input", mimic_input(["l"]))
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()
+    expected_o = "1. Ice cream - $10.0 - Units: 2\n2. Chocolate - $15.0 - Units: 5\n3. Popcorns - $8.0 - Units: 3"
+    assert expected_o in out[:96]
+
+def test_printout_add_item_to_cart(logout_stub1, capsys, monkeypatch):
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    cart = ShoppingCart()
+    products = [Product("Ice cream", 10, 2)]
+    monkeypatch.setattr("checkout_and_payment.products", products)
+    monkeypatch.setattr("checkout_and_payment.cart", cart)
+    monkeypatch.setattr("builtins.input", mimic_input(["1", "l", "y"]))
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()
+    expected_output = "Ice cream added to your cart."
+    assert expected_output in out[30:]
+
+def test_printout_item_out_of_stock(logout_stub1, capsys, monkeypatch):
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    cart = ShoppingCart()
+    products = [Product("Ice cream", 10, 0)]
+    monkeypatch.setattr("checkout_and_payment.products", products)
+    monkeypatch.setattr("checkout_and_payment.cart", cart)
+    monkeypatch.setattr("builtins.input", mimic_input(["1", "l", "y"]))
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()
+    expected_output = "Sorry, Ice cream is out of stock."
+    assert expected_output in out[30:]
+
+def test_printout_choice_other_letter(logout_stub1, capsys, monkeypatch):
     login_info = {"username": "Ramanathan", "wallet": 100}
     monkeypatch.setattr("builtins.input", mimic_input(["a", "l"]))
     checkoutAndPayment(login_info)
@@ -56,10 +95,82 @@ def test_choice_other_letter(logout_stub1, capsys, monkeypatch):
     expected_output = "Invalid input. Please try again."
     assert expected_output in out[2217:2249]
 
-def test_choice_other_number(capsys, monkeypatch):           #händer inget??
+def test_printout_choice_other_number(capsys, monkeypatch):
     login_info = {"username": "Ramanathan", "wallet": 100}
     monkeypatch.setattr("builtins.input", mimic_input(["72", "l"]))
     checkoutAndPayment(login_info)
     out, err = capsys.readouterr()
     expected_output = "Invalid input. Please try again."
     assert expected_output in out[2217:2249]
+
+
+
+
+
+def test_check_cart_empty_cart(logout_stub1, capsys, monkeypatch):
+    def check_cart_stub3(user, cart):
+        check_cart.append(cart.retrieve_item())
+        return False
+
+    check_cart = []
+    cart = ShoppingCart()
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    monkeypatch.setattr("checkout_and_payment.cart", cart)
+    monkeypatch.setattr("checkout_and_payment.products", [])
+    monkeypatch.setattr("builtins.input", mimic_input(["c", "l"]))
+
+    monkeypatch.setattr("checkout_and_payment.check_cart", check_cart_stub3)
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()  # just to get rid of outputs
+    assert check_cart == [[]]
+
+
+def test_check_cart_user_info(logout_stub1, capsys, monkeypatch):
+    def check_cart_stub3(user, cart):
+        check_user.append(user.name)
+        check_user.append(user.wallet)
+        return False
+
+    check_user = []
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    monkeypatch.setattr("checkout_and_payment.products", [])
+    monkeypatch.setattr("builtins.input", mimic_input(["c", "l"]))
+    monkeypatch.setattr("checkout_and_payment.check_cart", check_cart_stub3)
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()  # just to get rid of outputs
+    assert check_user[0] == "Ramanathan"
+    assert check_user[1] == 100
+
+def test_logout_empty_cart(capsys,monkeypatch):
+    def logout_stub2(cart):
+        logout_cart.append(cart.retrieve_item())
+        return True
+
+    logout_cart = []
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    product = []
+    monkeypatch.setattr("checkout_and_payment.products", product)
+    monkeypatch.setattr("builtins.input", mimic_input(["l"]))
+    monkeypatch.setattr("checkout_and_payment.logout", logout_stub2)
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()      #just to get rid of outputs
+    assert logout_cart == [[]]
+
+def test_logout_nonempty_cart(capsys, monkeypatch):
+    def logout_stub2(cart):
+        for item in cart.retrieve_item():
+            p = item.get_product()
+            logout_cart.append(p)
+        return True
+
+    logout_cart = []
+    cart = ShoppingCart()
+    login_info = {"username": "Ramanathan", "wallet": 100}
+    product = [Product("Ice cream", 10, 2)]
+    monkeypatch.setattr("checkout_and_payment.cart", cart)
+    monkeypatch.setattr("checkout_and_payment.products", product)
+    monkeypatch.setattr("builtins.input", mimic_input(["1","l","y"]))
+    monkeypatch.setattr("checkout_and_payment.logout", logout_stub2)
+    checkoutAndPayment(login_info)
+    out, err = capsys.readouterr()      #just to get rid of outputs
+    assert logout_cart == [['Ice cream', 10.0, 2]]
